f7d96e2a890a2fb6abdea6dda9e7b094bc6de2fd
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-04-28 12:32:57 +0600

    Fix : Fixed the seupWSConnection import for node 18.X -v

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-import express from 'express';
-import { WebSocketServer } from 'ws';
-import {createRequire} from "module" ;
-import { configDotenv } from 'dotenv';
+import express from "express";
+import { WebSocketServer } from "ws";
+import { createRequire } from "module";
+import { setupWSConnection } from "y-websocket/bin/utils";
+import { configDotenv } from "dotenv";
 
 configDotenv();
 
-const require = createRequire(import.meta.url); //used to force y-websocket to return utils
-const { setupWSConnection } = require('y-websocket/bin/utils');
-
 const app = express();
-const PORT = Number(process.env.PORT) || 1234 ;
+const PORT = Number(process.env.PORT) || 1234;
 
-const server = app.listen(PORT , () => {
-    console.log("Listening to the server at port:" , PORT);
+const server = app.listen(PORT, () => {
+  console.log("Listening to the server at port:", PORT);
 });
 
 const wss = new WebSocketServer({ server });
 
-wss.on('connection' , (ws , req) => {
-    setupWSConnection(ws , req);
-})
+wss.on("connection", (ws, req) => {
+  setupWSConnection(ws, req);
+});
